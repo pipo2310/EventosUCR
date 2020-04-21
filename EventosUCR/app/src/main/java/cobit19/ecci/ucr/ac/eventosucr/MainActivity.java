ca632f074c0e5ed7714ef406d6e4db20e7535d2c
Chris <cristian.bujan@yahoo.com>
2020-04-21 16:31:17 -0600

    Inicio de Proyecto

---
+++ EventosUCR/app/src/main/java/cobit19/ecci/ucr/ac/eventosucr/MainActivity.java
@@ -0,0 +1,14 @@
+package cobit19.ecci.ucr.ac.eventosucr;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
