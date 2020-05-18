0e9654f523ebd1e06b3e01494b8ba49833096c44
Katherine <katherinev192009@gmail.com>
2020-05-17 21:59:34 -0600

    Funcionalidad de Mapa

--- app/src/main/java/cobit19/ecci/ucr/ac/eventosucr/fragments/VistaEventoFragment.java
+++ app/src/main/java/cobit19/ecci/ucr/ac/eventosucr/fragments/VistaEventoFragment.java
@@ -7,7 +7,17 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.UiSettings;
+import com.google.android.gms.maps.model.CameraPosition;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 
 import java.text.DateFormat;
 
@@ -17,9 +27,10 @@ import cobit19.ecci.ucr.ac.eventosucr.R;
 /**
  * A simple {@link Fragment} subclass.
  */
-public class VistaEventoFragment extends Fragment {
+public class  VistaEventoFragment extends Fragment implements OnMapReadyCallback {
     View v;
     Evento evento;
+    GoogleMap VistaEventoMap;
 
     public VistaEventoFragment() {
     }
@@ -35,8 +46,23 @@ public class VistaEventoFragment extends Fragment {
         // Inflate the layout for this fragment
         v = inflater.inflate(R.layout.fragment_vista_evento, container, false);
         leerEvento();
+
+        SupportMapFragment mapFragment = (SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.ubicacionMap);
+        mapFragment.getMapAsync(this);
+
+        //Botón para que el usuario le de like a un evento
+        Button btnMeInteresa = (Button)v.findViewById(R.id.btnMeInteresa);
+
+        btnMeInteresa.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                DarMeGusta();
+            }
+        });
+
         return v;
     }
+
 
     //Inserta los datos de la base de datos en la vista de un evento
     void leerEvento() {
@@ -53,5 +79,50 @@ public class VistaEventoFragment extends Fragment {
         ubicacion.setText(evento.getUbicacion());
         TextView detalles = (TextView)v.findViewById(R.id.descripcionDetalles);
         detalles.setText(evento.getDetalles());
+    }
+
+    //Cuando el usuario le da click al botón, añade el evento a la lista de favoritos del usuario
+    public void DarMeGusta(){
+
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        configurarMapa(googleMap);
+    }
+
+    //Permite realizar la configuración del mapa. Añade las coordenadas y el marcador  del evento correspondiente
+    //Configura la cámara y el zoom
+    public void configurarMapa(GoogleMap googleMap){
+        VistaEventoMap = googleMap;
+
+        //Se agregan las coordenadas del mapa
+        LatLng ubicacionEvento = new LatLng(evento.getLatitud(), evento.getLongitud());
+
+        //Se agrega el marcador en el mapa
+        VistaEventoMap.addMarker(new MarkerOptions().position(ubicacionEvento).title(evento.getNombre()));
+
+        // Muevo el mapa hacia la posición del marcador
+        VistaEventoMap.moveCamera(CameraUpdateFactory.newLatLng(ubicacionEvento));
+
+        //Opciones de zoom
+        UiSettings mapSettings;
+        mapSettings = VistaEventoMap.getUiSettings();
+        mapSettings.setZoomControlsEnabled(true); //controles zoom
+        mapSettings.setZoomGesturesEnabled(true); //gestos zoom
+        mapSettings.setCompassEnabled(true); //brújula
+        mapSettings.setMyLocationButtonEnabled(true);
+        mapSettings.setRotateGesturesEnabled(true); //rotación
+
+
+        //Mover la cámara
+        CameraPosition cameraPosition = new CameraPosition.Builder()
+                .target(ubicacionEvento)
+                .zoom(50)
+                .bearing(70)
+                .tilt(25)
+                .build();
+        VistaEventoMap.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition));
+
     }
 }
