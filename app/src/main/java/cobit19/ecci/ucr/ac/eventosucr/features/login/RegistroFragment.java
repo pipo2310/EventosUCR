cd43ba08b6604c4c6c0d9d237feaf2c265467289
Christian <christian.rodriguezbujan@ucr.ac.cr>
2020-06-19 16:28:41 -0600

    CCB-55 Registro. Conexion a FireBase. D:Christian. N:Walter, Fabian

--- app/src/main/java/cobit19/ecci/ucr/ac/eventosucr/features/login/RegistroFragment.java
+++ app/src/main/java/cobit19/ecci/ucr/ac/eventosucr/features/login/RegistroFragment.java
@@ -3,25 +3,50 @@ package cobit19.ecci.ucr.ac.eventosucr.features.login;
 import android.content.Intent;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import cobit19.ecci.ucr.ac.eventosucr.MainActivity;
 import cobit19.ecci.ucr.ac.eventosucr.R;
 
 /**
- * A simple {@link Fragment} subclass.
+ * CCB-44 Registro
+ * Subtasks
+ *  CCB-53
+ *  CCB-54
+ *  CCB-55
+ *  Participantes:
+ *      Fabian Alvarez
+ *      Walter Bonilla
+ *      Christian Rodriguez
+ *
  */
 public class RegistroFragment extends Fragment {
     View view;
 
     // Boton de registro
+    Button registrar;
     TextView tieneCuentaBtn;
+    EditText correo;
+    EditText contrasenna;
+    EditText confirmarContrasenna;
+    private FirebaseAuth mAuth;
 
     public RegistroFragment() {
         // Required empty public constructor
@@ -34,8 +59,26 @@ public class RegistroFragment extends Fragment {
         // Inflate the layout for this fragment
         view = inflater.inflate(R.layout.fragment_registro, container, false);
 
+        mAuth = FirebaseAuth.getInstance();
+
+
         // Obtenemos de la vista el boton que indica que ya creo una cuenta
         tieneCuentaBtn = view.findViewById(R.id.registro_login);
+
+
+
+        correo=view.findViewById(R.id.registro_nombre_usuario);
+        contrasenna=view.findViewById(R.id.registro_contrasenna);
+        confirmarContrasenna=view.findViewById(R.id.registro_conf_contrasenna);
+        registrar=view.findViewById(R.id.registro_boton_is);
+
+        // Agregamos la accion que se quiere hacer cuando se presione el boton de registrar
+        registrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                registrarUsuario();
+            }
+        });
 
         // Agregamos la accion que se quiere hacer cuando se presione el boton de ya tiene cuenta
         tieneCuentaBtn.setOnClickListener(new View.OnClickListener() {
@@ -50,7 +93,27 @@ public class RegistroFragment extends Fragment {
         return view;
     }
 
-    public void registrar(){
+    public void registrarUsuario(){
+
+        //Validaciones to do
+        mAuth.createUserWithEmailAndPassword(correo.getText().toString(), contrasenna.getText().toString())
+                .addOnCompleteListener(getActivity(), new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Inicio de sesion exitoso
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            // Se envia al usuario a la vista principal
+                            //cambiarDePantalla(MainActivity.class);
+                        } else {
+                            // Si el inicio de sesion falla, se le indica al usuario
+                            Toast.makeText(getActivity(), "Usuario o contraseña incorrectos", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+
+                        // ...
+
+                });
 
     }
 
