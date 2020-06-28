7243108615f40fa0e1a8b33fece2e35a0bd5b18f
Walter <walter.bg7@gmail.com>
2020-06-28 14:21:23 -0600

    Comentarios. Cambios de PR, botón de like

--- app/src/main/java/cobit19/ecci/ucr/ac/eventosucr/features/vistaEvento/ComentariosListAdapter.java
+++ app/src/main/java/cobit19/ecci/ucr/ac/eventosucr/features/vistaEvento/ComentariosListAdapter.java
@@ -1,18 +1,15 @@
 package cobit19.ecci.ucr.ac.eventosucr.features.vistaEvento;
 
 import android.app.Activity;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
-import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.ListView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
-import androidx.recyclerview.widget.RecyclerView;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
@@ -21,7 +18,6 @@ import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.Query;
 import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
